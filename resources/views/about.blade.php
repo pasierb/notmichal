25c366ecf88ffe646278a9531c98452fbcbfafb5
Michal Pasierbski <mpasierbski@gmail.com>
2024-10-18 19:50:31 +0200

    Add employment history

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,8 @@
     <div class="flex flex-col items-center">
         <img src="{{ asset('images/notmichal.png') }}" alt="Michal Unfiltered" class="w-full max-w-xs mx-4 rounded-full">
     </div>
+
+    <div class="mt-8">
+        <x-employment-history />
+    </div>
 </x-landing-layout>
---
+++ resources/views/components/employment-history.blade.php
@@ -0,0 +1,72 @@
+<ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+    <li>
+    </li>
+    <li>
+        <div class="timeline-middle">
+            <x-fab-google class="w-8 h-8" />
+        </div>
+        <div class="timeline-start mb-10 md:text-end">
+            <time class="font-mono italic">Nov 2021 - Present</time>
+            <div class="text-lg font-black">Software Engineer at Google</div>
+            Zurich, Switzerland
+        </div>
+        <hr />
+    </li>
+    <li>
+        <hr />
+        <div class="timeline-middle">
+            <x-fab-aws class="w-8 h-8" />
+        </div>
+        <div class="timeline-end mb-10">
+            <time class="font-mono italic">
+                Aug 2020 - Oct 2021
+            </time>
+            <div class="text-lg font-black">Software Engineer at Amazon AWS</div>
+            Berlin, Germany
+        </div>
+        <hr />
+    </li>
+    <li>
+        <hr />
+        <div class="timeline-middle">
+            <img src="{{ asset('images/EF_logo.svg') }}" alt="EF Education First" class="w-8 h-8">
+        </div>
+        <div class="timeline-start mb-10 md:text-end">
+            <time class="font-mono italic">Jul 2019 - Jul 2020</time>
+            <div class="text-lg font-black">Lead Software Engineer at EF Education First</div>
+            Zurich, Switzerland
+        </div>
+        <hr />
+    </li>
+    <li>
+        <hr />
+        <div class="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5">
+                <circle cx="4" cy="12" r="2" />
+                <circle cx="12" cy="12" r="2" />
+                <circle cx="20" cy="12" r="2" />
+            </svg>
+        </div>
+        <div class="timeline-end mb-10">
+            <time class="font-mono italic">2010 - 2019</time>
+            <div class="text-lg font-black">Bunch of other companies...</div>
+            while living in Poland, Spain and Switzerland
+        </div>
+        <hr />
+    </li>
+    <li>
+        <hr />
+        <div class="timeline-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                <path fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="timeline-start mb-10 md:text-end">
+            <time class="font-mono italic">May 2009 - Sep 2010</time>
+            <div class="text-lg font-black">Java Developer at CMS Mirage</div>
+            Wroclaw, Poland
+        </div>
+    </li>
+</ul>
