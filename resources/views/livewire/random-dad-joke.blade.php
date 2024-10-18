a4f1d2ad815e1929db7a653f6b3da1e5a5e5ef75
Michal Pasierbski <mpasierbski@gmail.com>
2024-10-18 22:40:17 +0200

    Fix single root for livewire

--- resources/views/livewire/random-dad-joke.blade.php
+++ resources/views/livewire/random-dad-joke.blade.php
@@ -1,15 +1,17 @@
-@if ($joke)
-    <div class="card bg-white w-full max-w-2xl shadow-lg">
-        <div class="card-body">
-            <h2 class="card-title">Random dad joke</h2>
+<div>
+    @if ($joke)
+        <div class="card bg-white w-full max-w-2xl shadow-lg">
+            <div class="card-body">
+                <h2 class="card-title">Random dad joke</h2>
 
-            <div class="py-4 px-6 text-lg rounded-lg font-mono my-4 bg-black text-white">
-                {!! Illuminate\Support\Str::markdown($joke->text_content) !!}
-            </div>
+                <div class="py-4 px-6 text-lg rounded-lg font-mono my-4 bg-black text-white">
+                    {!! Illuminate\Support\Str::markdown($joke->text_content) !!}
+                </div>
 
-            <div class="card-actions justify-end">
-                <button wire:click="selectNewJoke" class="btn btn-primary">Another one</button>
+                <div class="card-actions justify-end">
+                    <button wire:click="selectNewJoke" class="btn btn-primary">Another one</button>
+                </div>
             </div>
         </div>
-    </div>
-@endif
+    @endif
+</div>
