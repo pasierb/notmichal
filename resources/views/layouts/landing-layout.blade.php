dffbee9355da1b201c0de87d488567a2024b1d60
Michal Pasierbski <mpasierbski@gmail.com>
2024-10-19 13:06:40 +0200

    Add google tracking code

--- resources/views/layouts/landing-layout.blade.php
+++ resources/views/layouts/landing-layout.blade.php
@@ -6,6 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CJGVJ1HHN1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-CJGVJ1HHN1');
+    </script>
+
     @if (isset($seo))
         {!! $seo !!}
     @else
